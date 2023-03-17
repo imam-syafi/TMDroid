5227c02b1af17629c5e4bd82639004a5a6bd25ee
imam-syafi <122346179+imam-syafi@users.noreply.github.com>
2023-03-18 00:48:34 +0700

    Display splash screen

---
+++ app/src/main/java/com/edts/tmdroid/SplashScreenActivity.kt
@@ -0,0 +1,37 @@
+package com.edts.tmdroid
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.edts.tmdroid.databinding.ActivitySplashScreenBinding
+import com.edts.tmdroid.ui.main.MainActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.setup()
+    }
+
+    private fun ActivitySplashScreenBinding.setup() {
+        logo
+            .animate()
+            .setDuration(1500)
+            .alpha(1f)
+            .withEndAction {
+                val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+                startActivity(intent)
+
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+
+                finish()
+            }
+
+    }
+}

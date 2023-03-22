373c0fa66d050ed29aaef73bb44de395ce35be86
imam-syafi <122346179+imam-syafi@users.noreply.github.com>
2023-03-22 17:54:54 +0700

    Load API key from `local.properties` file, like environment variables
    Append the following line to `local.properties` file:
    
    ```
    TMDB_API_KEY="your api key here"
    ```

--- app/src/main/java/com/edts/tmdroid/service/TmdbService.kt
+++ app/src/main/java/com/edts/tmdroid/service/TmdbService.kt
@@ -1,7 +1,7 @@
 package com.edts.tmdroid.service
 
+import com.edts.tmdroid.BuildConfig
 import com.edts.tmdroid.service.response.GetMoviesResponse
-import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -11,20 +11,19 @@ interface TmdbService {
 
     @GET("movie/top_rated")
     suspend fun getTopRatedMovies(
-        @Query("api_key") apiKey: String = API_KEY,
+        @Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY,
         @Query("language") language: String = LANGUAGE,
     ): GetMoviesResponse
 
     @GET("search/movie")
     suspend fun searchMovies(
         @Query("query") query: String,
-        @Query("api_key") apiKey: String = API_KEY,
+        @Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY,
         @Query("language") language: String = LANGUAGE,
     ): GetMoviesResponse
 
     companion object {
         private const val BASE_URL = "https://api.themoviedb.org/3/"
-        private const val API_KEY = "foo"
         private const val LANGUAGE = "en-US"
 
         private val retrofit = Retrofit.Builder()

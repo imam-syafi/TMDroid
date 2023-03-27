7e48279be0276c77cd1dd4ac6e72ef5b5ce53773
imam-syafi <122346179+imam-syafi@users.noreply.github.com>
2023-03-27 09:48:55 +0700

    Set up linter plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+import com.diffplug.gradle.spotless.SpotlessExtensionPredeclare
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     dependencies {
@@ -9,4 +12,40 @@ plugins {
     id("com.android.application") version "7.4.2" apply false
     id("com.android.library") version "7.4.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("com.diffplug.spotless") version "6.17.0"
+}
+
+subprojects {
+
+    afterEvaluate {
+        apply(plugin = "com.diffplug.spotless")
+
+        configure<SpotlessExtension> {
+            kotlin {
+                target("**/*.kt")
+                targetExclude("$buildDir/**/*.kt", "bin/**/*.kt")
+                ktlint("0.48.2")
+                trimTrailingWhitespace()
+                endWithNewline()
+            }
+
+            format("misc") {
+                target("**/*.kts", "**/*.gradle", "**/*.xml", "**/*.md", "**/.gitignore")
+                targetExclude("**/build/**/*.kts", "**/build/**/*.xml")
+                trimTrailingWhitespace()
+                indentWithSpaces(4)
+                endWithNewline()
+            }
+        }
+    }
+}
+
+configure<SpotlessExtension> {
+    predeclareDeps()
+}
+
+configure<SpotlessExtensionPredeclare> {
+    kotlin {
+        ktlint("0.48.2")
+    }
 }

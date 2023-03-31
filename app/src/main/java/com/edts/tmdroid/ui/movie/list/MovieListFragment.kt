8c9d565971a4ee515b5951b4cc003c794012d3b1
imam-syafi <122346179+imam-syafi@users.noreply.github.com>
2023-03-31 17:08:42 +0700

    Inject safe args and network service for movie list screen

--- app/src/main/java/com/edts/tmdroid/ui/movie/list/MovieListFragment.kt
+++ app/src/main/java/com/edts/tmdroid/ui/movie/list/MovieListFragment.kt
@@ -2,30 +2,18 @@ package com.edts.tmdroid.ui.movie.list
 
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.viewmodel.initializer
-import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
-import com.edts.tmdroid.data.remote.NetworkModule
 import com.edts.tmdroid.databinding.FragmentMovieListBinding
 import com.edts.tmdroid.ui.common.BaseFragment
 import com.edts.tmdroid.ui.ext.showDialog
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MovieListFragment : BaseFragment<FragmentMovieListBinding>(
     FragmentMovieListBinding::inflate,
 ) {
 
-    private val args by navArgs<MovieListFragmentArgs>()
-    private val viewModel by viewModels<MovieListViewModel> {
-        viewModelFactory {
-            initializer {
-                MovieListViewModel(
-                    args.movieListType,
-                    NetworkModule.tmdbService,
-                )
-            }
-        }
-    }
+    private val viewModel by viewModels<MovieListViewModel>()
 
     override fun FragmentMovieListBinding.setup() {
         swipeRefreshLayout.setOnRefreshListener {
--- app/src/main/java/com/edts/tmdroid/ui/movie/list/MovieListViewModel.kt
+++ app/src/main/java/com/edts/tmdroid/ui/movie/list/MovieListViewModel.kt
@@ -2,17 +2,23 @@ package com.edts.tmdroid.ui.movie.list
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.edts.tmdroid.data.remote.TmdbService
 import com.edts.tmdroid.data.remote.response.GetMoviesResponse
 import com.edts.tmdroid.ui.model.Movie
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 import kotlinx.coroutines.launch
 
-class MovieListViewModel(
-    movieListType: MovieListType,
+@HiltViewModel
+class MovieListViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
     tmdbService: TmdbService,
 ) : ViewModel() {
+
+    private val args = MovieListFragmentArgs.fromSavedStateHandle(savedStateHandle)
 
     private val _state = MutableLiveData(MovieListState())
     val state: LiveData<MovieListState> = _state
@@ -20,12 +26,12 @@ class MovieListViewModel(
     private val call: suspend () -> GetMoviesResponse
 
     init {
-        call = when (movieListType) {
+        call = when (val type = args.movieListType) {
             MovieListType.TopRated -> tmdbService::getTopRatedMovies
             MovieListType.Upcoming -> tmdbService::getUpcomingMovies
             MovieListType.NowPlaying -> tmdbService::getNowPlayingMovies
             MovieListType.Popular -> tmdbService::getPopularMovies
-            is MovieListType.Search -> suspend { tmdbService.searchMovies(movieListType.query) }
+            is MovieListType.Search -> suspend { tmdbService.searchMovies(type.query) }
         }
 
         fetchData()

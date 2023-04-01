592f7689e14a1f38a2079092b1c5aaff9f3861c8
imam-syafi <122346179+imam-syafi@users.noreply.github.com>
2023-04-01 15:42:19 +0700

    Set image placeholder

--- app/src/main/java/com/edts/tmdroid/ui/ext/ImageView.kt
+++ app/src/main/java/com/edts/tmdroid/ui/ext/ImageView.kt
@@ -1,10 +1,13 @@
 package com.edts.tmdroid.ui.ext
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 
 fun ImageView.loadFromUrl(url: String) {
     Glide.with(this)
         .load(url)
+        .placeholder(ColorDrawable(Color.GRAY))
         .into(this)
 }
